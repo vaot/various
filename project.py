15bdad37795c67cf4768a9d2f34d62bfd691aa90
vaot <victorandrey_123@hotmail.com>
2013-10-27 16:22:16 -0700

    add instructions and explaining

--- project.py
+++ project.py
@@ -1,11 +1,44 @@
 
 from graphics import *
-win = GraphWin("My Circle", 900, 506)
+win = GraphWin("The Family", 900, 506)
 
 GENERAL_YELLOW = color_rgb(249,221,4)
 DEFAULT_SKIN = color_rgb(251,217,180)
 
-class CommomParts(object):
+"""
+The Family
+
+Documentation and Understanding :
+
+There are 3 characters, they have their own characteristics and some that are shared.
+They will be instatiated at the bottom of the file, calling the draw_self function
+
+You can check the code out on GitHub : https://github.com/vaot/various/blob/master/project.py
+
+1 - Parent Class
+
+    * Provide all the commom characteristics among them
+
+    * All methods are scalable in terms of X and Z in the coordinate
+    * meaning that each feature can be scaled to fit and be placed at the right
+    * spot in the board.
+
+    * Most of all features provide the scalability through an array
+    * and the length of that array will depend on the feature
+    * Child classes (each character) will then implement its own feature
+    * inheriting from the super class('Parent') and specifing the scalability(Xs,Ys) in the board
+
+2 - Child Classes (Bob, Nick, Carlos)
+
+    * They will all have their features
+    * and provide the right scalability values for those
+    * who are shared among them
+
+    * Each child provide draw_'child_name' function
+    * that function will be responsible for order of execution
+
+"""
+class Parent(object):
 
     def arm(self, array, array2, color):
         pol = Polygon(Point(array[0] + 202, array2[0] + 356),
@@ -17,20 +50,20 @@ class CommomParts(object):
                       Point(array[6] + 184,array2[6] + 420),
                       Point(array[7] + 224,array2[7] + 424),
                       Point(array[8] + 244,array2[8] + 436))
-        
+
         pol.setFill(color)
         pol.setWidth(0)
         pol.draw(win)
-        
+
     def arm_right(self, array, array2, color):
         pol = Polygon(Point(318 + array[0],353 + array2[0]),
                       Point(350 + array[1],411 + array2[1]),
                       Point(350 + array[2],434 + array2[2]),
                       Point(315 + array[3],450 + array2[3]))
-        
+
         pol.setFill(color)
         pol.draw(win)
-        
+
     def hands(self, array , array2, color):
         circle = Circle(Point(187 + array[0],
                               420 + array2[0]),20)
@@ -57,10 +90,10 @@ class CommomParts(object):
                       Point(200 + array1[1] - scale_size,444 + array2[1]),
                       Point(193 + array1[2] - scale_size, 462 + array2[2]),
                       Point(80 + array1[3], 462 + array2[3]))
-        
+
         pol.setFill(color)
         pol.draw(win)
-        
+
     def feet(self, color, scalex = 0, scalex2 = 0, scaley = 0, scaley2 = 0):
         limit = 128 + scalex
         x1,x2 = (83 + scalex), (141 + scalex2)
@@ -70,10 +103,10 @@ class CommomParts(object):
             oval.setFill(color)
             oval.draw(win)
             x1 += 45
-            x2 += 45 
-        
-class Bob(CommomParts):
-    
+            x2 += 45
+
+class Bob(Parent):
+
     def hood(self):
         pt1 = Point(138,334)
         head = Circle(pt1,86)
@@ -81,7 +114,7 @@ class Bob(CommomParts):
         head.draw(win)
         circle = Circle(Point(135,244), 9)
         circle.setFill(GENERAL_YELLOW)
-        circle.draw(win) 
+        circle.draw(win)
 
 
     def make_face(self):
@@ -115,7 +148,7 @@ class Bob(CommomParts):
         circle.draw(win)
         line = Line(Point(127,384), Point(150,386))
         line.draw(win)
-        
+
     def body(self):
         pol = Polygon(Point(62,356), Point(48,366), Point(35,383), Point(59,392), Point(67,408), Point(41,411), Point(44,420), Point(84,424), Point(84,356))
         pol.setFill(color_rgb(212,31,64))
@@ -131,7 +164,7 @@ class Bob(CommomParts):
         lin.draw(win)
         y1 = 420
         y2 = 425
-        while y1 <= 449:  
+        while y1 <= 449:
             lin2 = Line(Point(140,y1), Point(140,y2))
             lin2.setWidth(2)
             lin2.draw(win)
@@ -148,9 +181,9 @@ class Bob(CommomParts):
 
     def low_body(self):
         super(Bob, self).low_body([0]*4, [0]*4, color_rgb(157,95,82))
-        super(Bob, self).feet('black')   
+        super(Bob, self).feet('black')
 
-    def draw_bob(self):
+    def draw_self(self):
         self.low_body()
         self.body()
         self.hand()
@@ -161,9 +194,9 @@ class Bob(CommomParts):
         self.closed_eyes('invert!')
         self.mouth()
         self.decoration()
-        
-class Nick(CommomParts):
-    
+
+class Nick(Parent):
+
     def hood(self):
         rec = Rectangle(Point(210,269), Point(333,329))
         rec.setFill(color_rgb(34,154,56))
@@ -178,11 +211,11 @@ class Nick(CommomParts):
             each.setWidth(0)
             each.setFill(color_rgb(87,197,38))
             each.draw(win)
-            
+
     def head(self):
         self.outerhead()
         super(Nick, self).head()
-        
+
     def outerhead(self):
         circle = Circle(Point(273,373), 65)
         circle.setFill(color_rgb(34,154,56))
@@ -222,27 +255,27 @@ class Nick(CommomParts):
             rec.draw(win)
             x1 += 6
             x2 += 6
-            
+
     def body(self):
         super(Nick, self).body(color_rgb(254,97,16))
-        
+
     def hands(self):
         super(Nick, self).hands([0], [0], color_rgb(87,197,38))
         super(Nick, self).hands([160], [0], color_rgb(87,197,38))
-        
+
     def arm(self):
         self.hands()
         super(Nick, self).arm([0]*9,[22]*9, color_rgb(254,97,16))
-    
+
 
     def arm_right(self):
         super(Nick, self).arm_right([0]*9,[0]*9, color_rgb(254,97,16))
-        
+
     def low_body(self):
         super(Nick, self).low_body([150]*4, [10]*4, color_rgb(47,79,76), 30)
         super(Nick, self).feet('black',135,135,10,10)
-        
-    def draw_nick(self):
+
+    def draw_self(self):
         self.low_body()
         self.arm()
         self.arm_right()
@@ -259,7 +292,7 @@ class Carlos(CommomParts):
     def head(self):
         super(Carlos, self).head(150)
 
-    def mohawk(self):    
+    def mohawk(self):
         pol = Polygon(Point(421,249),Point(450,296),Point(421,319),Point(396,297))
         pol.setFill('black')
         pol.draw(win)
@@ -316,8 +349,8 @@ class Carlos(CommomParts):
     def low_body(self):
          super(Carlos, self).low_body([300]*4, [10]*4, color_rgb(80,98,162), 30)
          super(Carlos, self).feet(color_rgb(212,31,64),285,285,10,10)
-         
-    def draw_carlos(self):
+
+    def draw_self(self):
         self.low_body()
         self.arm()
         self.arm_right()
@@ -329,14 +362,14 @@ class Carlos(CommomParts):
         self.mouth()
         self.hands()
         self.decoration()
-    
-        
+
+
 new_bob = Bob()
-new_bob.draw_bob()
+new_bob.draw_self()
 
 new_carlos = Carlos()
-new_carlos.draw_carlos()
+new_carlos.draw_self()
 
 new_nick = Nick()
-new_nick.draw_nick()
+new_nick.draw_self()
 
